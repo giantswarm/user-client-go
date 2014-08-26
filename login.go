c6ea3130acb7e33075123d6794e1c83408804c45
Tim Schindler <tim@catalyst-zero.com>
2014-08-26 19:15:22 +0000

    added login functionallity

---
+++ login.go
@@ -0,0 +1,16 @@
+package client
+
+import (
+	"net/http"
+
+	"github.com/juju/errgo"
+)
+
+func (this *Client) Login(userOrMail string, reqBody io.Reader) (*http.Response, error) {
+	res, err := this.post(this.endpointUrl("/user/"+userOrMail+"/login"), "application/json", reqBody)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+
+	return res, nil
+}
