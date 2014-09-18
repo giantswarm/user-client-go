14541aec3899a9c3f9c289d63abf5be34dc42f0d
Stephan Zeissler <stephan@dockzero.io>
2014-09-18 14:03:16 +0200

    Made the parameters to CreateUser() explicit.

--- create.go
+++ create.go
@@ -1,14 +1,19 @@
 package client
 
 import (
-	"io"
 	"net/http"
 
 	"github.com/juju/errgo"
 )
 
-func (this *Client) Create(reqBody io.Reader) (*http.Response, error) {
-	res, err := this.post(this.endpointUrl("/user/"), "application/json", reqBody)
+func (this *Client) Create(username, email, password string) (*http.Response, error) {
+	payload := map[string]string{
+		"username": username,
+		"password": password,
+		"email":    email,
+	}
+
+	res, err := this.postJson(this.endpointUrl("/user/"), payload)
 	if err != nil {
 		return nil, errgo.Mask(err)
 	}
