d4d73c9e747e15d45900bc0e09b7a52ba70984cd
Stephan Zeissler <stephan@giantswarm.io>
2016-06-28 13:09:41 +0200

    Add ResetPassword() function.

--- update_password.go
+++ update_password.go
@@ -1,16 +1,30 @@
 package client
 
 import (
-	"github.com/giantswarm/api-schema"
+	apischema "github.com/giantswarm/api-schema"
 )
 
-func (this *Client) UpdatePassword(userId, oldPass, newPass string) error {
+// UpdatePassword updates the password for the given user to newPass.
+// oldPass must contain the current password, otherwise an error is thrown.
+func (client *Client) UpdatePassword(userID, oldPass, newPass string) error {
 	payload := map[string]string{
 		"old_password": oldPass,
 		"new_password": newPass,
 	}
+	return client.postPasswordUpdate(userID, payload)
+}
 
-	resp, err := this.postSchemaJSON("/user/"+userId+"/password/update", payload)
+// ResetPassword resets the password of the given user to the new password.
+// No verification will be done.
+func (client *Client) ResetPassword(userID, newPass string) error {
+	payload := map[string]string{
+		"new_password": newPass,
+	}
+	return client.postPasswordUpdate(userID, payload)
+}
+
+func (client *Client) postPasswordUpdate(userID string, payload map[string]string) error {
+	resp, err := client.postSchemaJSON("/user/"+userID+"/password/update", payload)
 	if err != nil {
 		return Mask(err)
 	}
